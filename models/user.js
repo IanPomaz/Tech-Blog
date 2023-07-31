a4c2d6a3ac24666a77da47cb96f4770339610891
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-07-31 18:08:55 -0400

    user.js models

--- models/user.js
+++ models/user.js
@@ -1,2 +1,52 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password)
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes. STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate(userData) {
+                userData.password = await bcrypt.hash(userData.password, 10);
+                return userData;
+            },
+            
+            async beforeUpdate(userData) {
+                userData.password = await bcrypt.hash(userData.password, 10);
+                return userData;
+            },
+            
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'User'
+    }
+);
+
+module.exports = User;
