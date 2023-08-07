2338f1533b480b7e26ba73d4f3ea3c933ec56e62
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-08-07 17:56:46 -0400

    server  and models

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const User = require("./user");
+const Post = ("./post");
+const Comment = ("./comment");
+
+Post.belongsTo(User,{
+    foreignkey:"userId",
+    onDelete:"Cascade",
+});
+
+Post.hasMany(Comment,{
+    foreignkey:"postId",
+    onDelete:"Cascade",
+});
+
+Comment.belongsTo(User,{
+    foreignkey:"userId",
+    onDelete:"Cascade",
+});
+
+module.exports={User,Comment,Post};
+
+
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const sequelize = require("./config/connection");
+
+const sess = {
+    secret:"you dont know my secret!!!!",
+    cookie:{},
+    resave: false,
+    saveUninitialize: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
+
+// app.use(require("./controllers"));
+
+sequelize.sync().then(()=>app.listen(PORT,()=>console.log(`Now listening on http://localhost:${PORT}`)));
+
