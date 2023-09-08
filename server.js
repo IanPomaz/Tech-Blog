77a43b8bd478693f535f0e4a548dcd149f8d8566
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-09-08 14:41:14 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const sequelize = require("./config/connection");
+// const sequelize = require("./config/connection");
 
 const sess = {
-    secret:"you dont know my secret!!!!",
-    cookie:{},
-    resave: false,
-    saveUninitialize: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: "you dont know my secret!!!!",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  // store: new SequelizeStore({
+  //   db: sequelize,
+  // }),
 };
 
 app.use(session(sess));
@@ -24,10 +24,18 @@ app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.use(require("./controllers"));
 
-sequelize.sync().then(()=>app.listen(PORT,()=>console.log(`Now listening on http://localhost:${PORT}`)));
+// Database causing issues with heroku
+// sequelize.sync().then(() => app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`)));
+
+app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
+
+
+
+
+
 
